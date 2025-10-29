4df7c57a62d8102dfe8581d33fb86b7fbf77c5b4
Abhishek_CodeHub <bt22cse131@iiitn.ac.in>
2025-10-29 23:58:43 +0530

    import changes

--- src/test/java/com/example/College_Management_Portal/Service/CourseTest.java
+++ src/test/java/com/example/College_Management_Portal/Service/CourseTest.java
@@ -1,8 +1,6 @@
 package com.example.College_Management_Portal.Service;
 
 import java.util.List;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
