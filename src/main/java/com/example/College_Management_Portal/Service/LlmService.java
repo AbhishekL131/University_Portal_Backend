adf94f081db4b85e1f8c8f40910e1d0814777d4f
Abhishek_CodeHub <bt22cse131@iiitn.ac.in>
2025-12-19 19:46:14 +0530

    changes to LLM-Service

--- src/main/java/com/example/College_Management_Portal/Service/LlmService.java
+++ src/main/java/com/example/College_Management_Portal/Service/LlmService.java
@@ -1,6 +1,5 @@
 package com.example.College_Management_Portal.Service;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
@@ -11,11 +10,12 @@ public class LlmService {
     
     private final OpenAiChatModel model;
 
+    
+
     public LlmService(OpenAiChatModel model){
         this.model = model;
     }
 
-    @Cacheable(value="ans",key="#prompt")
     public String generateText(String prompt){
         return model.generate(prompt);
     } 
