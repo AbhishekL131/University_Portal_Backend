c44f07ad127b2e8dc26f91b1679985722c3fa78c
Abhishek_CodeHub <bt22cse131@iiitn.ac.in>
2025-12-21 19:47:17 +0530

    CORS configuration

--- src/main/java/com/example/College_Management_Portal/SecurityConfig/SpringSecurity.java
+++ src/main/java/com/example/College_Management_Portal/SecurityConfig/SpringSecurity.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import com.example.College_Management_Portal.Filter.JwtFilter;
 
 
@@ -54,6 +55,20 @@ public class SpringSecurity {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception{
         return auth.getAuthenticationManager();
     }
+
+
+ /* 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Vite default port
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     
-    
+    */
 }
--- src/test/java/com/example/College_Management_Portal/CollegeManagementPortalApplicationTests.java
+++ src/test/java/com/example/College_Management_Portal/CollegeManagementPortalApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CollegeManagementPortalApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
